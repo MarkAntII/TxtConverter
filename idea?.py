4baf346ec2ac35f7c1d10a2acaed58ec6a682b78
MarkAntII <148059516+MarkAntII@users.noreply.github.com>
2023-10-16 18:01:19 +0300

    Add files via upload

--- idea?.py
+++ idea?.py
@@ -1,16 +1,16 @@
 import os
 
-problemTxt = '/problems.txt' #Файл с текстами задачи
+problemTxt = 'problems.txt' #Файл с текстами задачи
 
-tempF = '/ILastRes.txt' #Место временного файла
+tempF = 'LastRes.txt' #Место временного файла
 
-outputF = '/AllreadyConverted/txt.txt' #Место итогого файла 
+outputF = 'AllreadyConverted/txt.txt' #Место итогого файла 
 
-dir = '/ForConvert/'   #Дириктория с кодом !Может добавлю какойто метод вовода задач но для этого нужен pdf учебника :(
+dir = 'ForConvert/'   #Дириктория с кодом !Может добавлю какойто метод вовода задач но для этого нужен pdf учебника :(
 
 
 def calcIO(f, n):  #Сохранение результата программы (f)   n раз в временный файл tempF 
-    
+    print('Выполняю '+ f + ' Кол-во раз ' + str(n) )
     for i in range(n):
         os.system('python3 '+f+' >> ' + tempF)
     
@@ -21,46 +21,24 @@ def calcIO(f, n):  #Сохранение результата программы
 
 edfile = open(outputF, 'w+')        #Открывает итоговый файл с "готовой" работой для редактирования 
 
-#for file in os.listdir(dir):
-    #x = open(dir+file).readlines()
-    
-    #dfile.write('  Текст задачи \n')
-    
-    #edfile.write(''.join(open(problemTxt).readlines()).split('@$\n')[os.listdir(dir).index(file)] + '\n')
-    
-    #edfile.write('  Код программы \n')
-    
-    #for i in x:
-        #edfile.write(i)
-
-    #edfile.write('\n')
-
-
-    #edfile.write('  Результат работы программы \n')
-
-    #Пока не разобрался как брать инпут юзера в отдельный файл
-
-    #edfile.write(calcIO(dir + file, int(input('Сколько раз запускать прорамму ?  '))))
-
-
 
 for file in os.listdir(dir):
     
     x = open(dir+file).readlines()
     txt = ''
 
-    txt += ('  Текст задачи \n')
+    txt += ('  Текст задачи \n \n')
 
     txt +=(''.join(open(problemTxt).readlines()).split('@$\n')[os.listdir(dir).index(file)] + '\n')
 
-    txt +=('  Код программы \n')
+    txt +=('  Код программы \n \n')
     
     for i in x:
         txt+=(i)
     txt +=('\n')
 
 
-    txt +=('  Результат работы программы \n')
+    txt +=('  Результат работы программы \n  input/output  \n')
 
     #Пока не разобрался как брать инпут юзера в отдельный файл
 
@@ -70,6 +48,6 @@ for file in os.listdir(dir):
     edfile.write(txt)
 
 
-
+print('Файл готов расположен в '+ outputF)
 
 
